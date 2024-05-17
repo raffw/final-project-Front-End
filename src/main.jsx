810cee32dfb3aa21dc7cd69c45ae1c8d303f2726
project.teguh <project.teguh@gmail.com>
2024-05-17 16:47:12 +0700

    add routes in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import './App.jsx'
+import { createBrowserRouter, RouterProvider }  from "react-router-dom"
+
+import Homepages from "./pages/Homepages";
+// import LoginPage from "./pages/login";
+// import RegisterPage from "./pages/register";
+import ErrorPage from "./pages/404";
+// import ProductsPage from './pages/products';
+// import CheckoutPage from './pages/checkout';
+// import DetailProductPage from './pages/product';
+// import CardSpecification from './components/Fragments/CardSpecification';
+// import CardSeller from './components/Fragments/CardSeller';
+// import CardItem from './components/Fragments/CardItem';
+
+
+
+const router = createBrowserRouter([
+// {
+//     path: "/",
+//     element: <div>Hello there</div>,
+//     errorElement: <ErrorPage />,
+// },
+{
+    path: "/home",
+    rElement: <Homepages />,
+}
+// {
+//   path: "/login",
+//   element: <LoginPage/>,
+// },
+// {
+//   path: "/register",
+//   element: <RegisterPage/>,
+// },
+// {
+//   path: "/products",
+//   element: <ProductsPage/>,
+// },
+// {
+//   path: "/checkout",
+//   element: <CheckoutPage/>,
+// },
+// {
+//   path: "/product",
+//   element: <DetailProductPage/>,
+// },
+// {
+//   path: "/specification",
+//   element: <CardSpecification/>,
+// },
+// {
+//   path: "/item",
+//   element: <CardItem/>,
+// },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
