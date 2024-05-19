23422355cba2acae019514ab549ae197f7fa3437
project.teguh <project.teguh@gmail.com>
2024-05-19 08:09:22 +0700

    add products page files for FE

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Homepages from "./pages/Homepages";
 import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import ErrorPage from "./pages/404";
-// import ProductsPage from './pages/products';
+import ProductsPage from './pages/products';
 import CheckoutPage from './pages/checkout';
 import DetailProductPage from './pages/product';
 // import CardSpecification from './components/Fragments/CardSpecification';
@@ -18,14 +18,14 @@ import DetailProductPage from './pages/product';
 
 
 const router = createBrowserRouter([
-// {
-//     path: "/",
-//     element: <div>Hello there</div>,
-//     errorElement: <ErrorPage />,
-// },
+
 {
     path: "/",
     element: <Homepages/>,
+},
+{
+  path: "/404",
+  element: <ErrorPage/>,
 },
 {
   path: "/login",
@@ -35,10 +35,10 @@ const router = createBrowserRouter([
   path: "/register",
   element: <RegisterPage/>,
 },
-// {
-//   path: "/products",
-//   element: <ProductsPage/>,
-// },
+{
+  path: "/products",
+  element: <ProductsPage/>,
+},
 {
   path: "/checkout",
   element: <CheckoutPage/>,
@@ -47,14 +47,6 @@ const router = createBrowserRouter([
   path: "/product",
   element: <DetailProductPage/>,
 },
-// {
-//   path: "/specification",
-//   element: <CardSpecification/>,
-// },
-// {
-//   path: "/item",
-//   element: <CardItem/>,
-// },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
